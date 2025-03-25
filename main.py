61eb9745aaf398c5cb500997c5349da715fa8ed4
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-25 21:04:19 +0530

    binary threshold 170, qr works, lol

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import tkinter as tk
 from tkinter import PhotoImage
 image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE)
 # image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-th, im_th_gray_otsu = cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+th, im_th_gray = cv2.threshold(image,170,255,cv2.THRESH_BINARY)
 print(th)
-cv2.imwrite('qr2.png', im_th_gray_otsu)
-# grayscale and otsu applied above.
+cv2.imwrite('qr2.png', im_th_gray)
+# otsu bad
 
+# 170 b/w
