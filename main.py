953b044b228473f43ac64a6c92c58362a896ed2a
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-26 19:59:07 +0530

    edge detection

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE)
 # image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 th, im_th_gray = cv2.threshold(image,170,255,cv2.THRESH_BINARY)
 print(th)
-contours, hierarchy = cv2.findContours(im_th_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-im_color = cv2.cvtColor(im_th_gray, cv2.COLOR_GRAY2BGR)
-cv2.drawContours(im_color, contours, -1, (0, 0, 255), 1)
-cv2.imwrite('qr2.png', im_color)
+# contours, hierarchy = cv2.findContours(im_th_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# im_color = cv2.cvtColor(im_th_gray, cv2.COLOR_GRAY2BGR)
+# cv2.drawContours(im_color, contours, -1, (0, 0, 255), 1)
+edges = cv2.Canny(image=im_th_gray, threshold1=100, threshold2=200)
+cv2.imwrite('qr2.png', edges)
+
 # otsu bad 170 b/w
 pygame.init()
 x = 500
