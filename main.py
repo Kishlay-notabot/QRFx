75edbd55b058bb878dfc91d188a847dcac490cf0
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-26 11:24:03 +0530

    not using pillow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import pygame
+from PIL import Image 
 image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE)
 # image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 th, im_th_gray = cv2.threshold(image,170,255,cv2.THRESH_BINARY)
@@ -11,7 +12,12 @@ x = 1000
 y = 700
 dsp = pygame.display.set_mode((x, y))
 pygame.display.set_caption('QRFx')
-img_dp = pygame.image.load("qr2.png").convert()
+pil_image = Image.open('qr2.png')
+h = pil_image.size[0]
+w = pil_image.size[1]
+pil_image = pil_image.resize((w-10,h-10), Image.ANTIALIAS)
+pil_image.save('qr_c.png')
+img_dp = pygame.image.load("qr_c.png").convert()
 dsp.blit(img_dp, (0, 0))
 pygame.display.flip()
 status = True
