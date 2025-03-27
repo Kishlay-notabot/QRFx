d39748423ba1765c611909ab009aaf25f23532d8
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-27 22:09:01 +0530

    tweak parameters, both in bilateral and canny edge

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import cv2
 # import pygame
 import sys
 import numpy as np
-image = cv2.imread('qr2.jpeg', cv2.IMREAD_GRAYSCALE)
-threshold1 = int(sys.argv[1]) if len(sys.argv) > 1 else 80
-threshold2 = int(sys.argv[2]) if len(sys.argv) > 2 else 200
+image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE)
+threshold1 = int(sys.argv[1]) if len(sys.argv) > 1 else 50
+threshold2 = int(sys.argv[2]) if len(sys.argv) > 2 else 100
+blur = cv2.bilateralFilter(image,9,75,75)
 # image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 # th, im_th_gray = cv2.threshold(image,170,255,cv2.THRESH_BINARY)
 # print(th)
 # contours, hierarchy = cv2.findContours(im_th_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # im_color = cv2.cvtColor(im_th_gray, cv2.COLOR_GRAY2BGR)
 # cv2.drawContours(im_color, contours, -1, (0, 0, 255), 1)
-edges = cv2.Canny(image=image, threshold1=threshold1, threshold2=threshold2)
-# blur = cv2.blur(edges,(5,5))
+edges = cv2.Canny(image=blur, threshold1=threshold1, threshold2=threshold2)
+
 cv2.imwrite('qr2.png', edges)
 # try gaussian blur blurred_img = cv2.GaussianBlur(img, (5,5), 0)
 # bilateral filter is maybe what i exactly want. also read about filling contour gaps
