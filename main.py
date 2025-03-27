7f49b0d8058843d00b3061a4fba1680916a24082
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-27 21:39:20 +0530

    dual exposure?

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import pygame
+# import pygame
 import sys
 import numpy as np
 image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE)
@@ -12,8 +12,8 @@ threshold2 = int(sys.argv[2]) if len(sys.argv) > 2 else 200
 # im_color = cv2.cvtColor(im_th_gray, cv2.COLOR_GRAY2BGR)
 # cv2.drawContours(im_color, contours, -1, (0, 0, 255), 1)
 edges = cv2.Canny(image=image, threshold1=threshold1, threshold2=threshold2)
-blur = cv2.blur(edges,(5,5))
-cv2.imwrite('qr2.png', blur)
+# blur = cv2.blur(edges,(5,5))
+cv2.imwrite('qr2.png', edges)
 # try gaussian blur blurred_img = cv2.GaussianBlur(img, (5,5), 0)
 # bilateral filter is maybe what i exactly want. also read about filling contour gaps
 # otsu bad 170 b/w
