43068cac6f884c4921c4dcc8f3cd86480c165818
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-28 12:42:14 +0530

    multi pass gaussian blur doesnt do the trick

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import numpy as np
 image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE)
 threshold1 = int(sys.argv[1]) if len(sys.argv) > 1 else 50
 threshold2 = int(sys.argv[2]) if len(sys.argv) > 2 else 100
-blurred_img = cv2.GaussianBlur(image, (5,5), 0)
+
+for _ in range(5):
+    blurred_img = cv2.GaussianBlur(image, (5,5), 0)
 
 # image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 # th, im_th_gray = cv2.threshold(image,170,255,cv2.THRESH_BINARY)
@@ -16,7 +18,7 @@ blurred_img = cv2.GaussianBlur(image, (5,5), 0)
 edges = cv2.Canny(image=blurred_img, threshold1=threshold1, threshold2=threshold2)
 
 cv2.imwrite('qr2.png', edges)
-# try gaussian blur blurred_img = cv2.GaussianBlur(img, (5,5), 0)
+# tried gaussian blur. FILLING is the only option. maybe
 # bilateral filter didnt work
 # white fill, floodfill.
 # otsu bad 170 b/w
