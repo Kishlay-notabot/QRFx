c4b7bae8efecded231f55c01308d363042a541ea
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-28 12:26:13 +0530

    gaussian blur 30 20

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# QRFx
-
+# QRFx - finding the best way to waste my time
+## i mean i made this for the FR8 selection round 2... a simple project
 this project is not made to fix *any* broken qr image.  
 i haven't taken generalization of the algo into consideration. I solely made this project to fix a qr code (single) I already had which wasn't working  
 and i wanted to fix it and have it functioning properly, also create a digitized version of it.  
--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import numpy as np
 image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE)
 threshold1 = int(sys.argv[1]) if len(sys.argv) > 1 else 50
 threshold2 = int(sys.argv[2]) if len(sys.argv) > 2 else 100
-blur = cv2.bilateralFilter(image,9,75,75)
-blur = cv2.bilateralFilter(blur,9,75,75)
+blurred_img = cv2.GaussianBlur(image, (5,5), 0)
 
 # image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 # th, im_th_gray = cv2.threshold(image,170,255,cv2.THRESH_BINARY)
@@ -14,12 +13,12 @@ blur = cv2.bilateralFilter(blur,9,75,75)
 # contours, hierarchy = cv2.findContours(im_th_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # im_color = cv2.cvtColor(im_th_gray, cv2.COLOR_GRAY2BGR)
 # cv2.drawContours(im_color, contours, -1, (0, 0, 255), 1)
-edges = cv2.Canny(image=blur, threshold1=threshold1, threshold2=threshold2)
+edges = cv2.Canny(image=blurred_img, threshold1=threshold1, threshold2=threshold2)
 
 cv2.imwrite('qr2.png', edges)
 # try gaussian blur blurred_img = cv2.GaussianBlur(img, (5,5), 0)
-# bilateral filter is maybe what i exactly want. also read about filling contour gaps
-# 3rd smoothing technique, white fill, floodfill.
+# bilateral filter didnt work
+# white fill, floodfill.
 # otsu bad 170 b/w
 # https://answers.opencv.org/question/53548/gap-filling-contours-lines/
 # https://stackoverflow.com/questions/26586123/filling-gaps-in-shape-edges
