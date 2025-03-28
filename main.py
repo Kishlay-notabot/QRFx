3caaf94b57bb421d51b34774b38160e146bf4e4a
kishlay-notabot <kishlay.work1@gmail.com>
2025-03-28 20:36:29 +0530

    cleaned files

--- main.py
+++ main.py
@@ -17,22 +17,25 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     return cv2.resize(image, dim, interpolation=inter)
 
 
-image = cv2.imread('qr.png', cv2.IMREAD_GRAYSCALE) # read in grayscale
+image = cv2.imread('source.png', cv2.IMREAD_GRAYSCALE) # read in grayscale
 threshold1 = int(sys.argv[1]) if len(sys.argv) > 1 else 50
 threshold2 = int(sys.argv[2]) if len(sys.argv) > 2 else 100
 blt = image.copy()
 for _ in range (5):
-    blt = cv2.GaussianBlur(blt,(5,5),0) # gaussian 
+    blt = cv2.GaussianBlur(blt,(5,5),0) # gaussian blur
 
 resized = ResizeWithAspectRatio(blt, width=400)
 cv2.imshow('gaussian on grayscale',resized)
 cv2.waitKey(500)
 th, fn = cv2.threshold(blt,170,255,cv2.THRESH_BINARY) # binarization of 5x gaussian blur
+cv2.imwrite('binarized.png', fn) # write binarized
+binarez = ResizeWithAspectRatio(fn, width=400)
+cv2.imshow('binarized', binarez)
 edges = cv2.Canny(image=fn, threshold1=threshold1, threshold2=threshold2) # canny edge detection 
 ed_res = ResizeWithAspectRatio(edges, width=400)
-cv2.imwrite('qr2.png', edges)
+cv2.imwrite('result.png', edges) # write canny edged
 cv2.imshow('edges',ed_res)
-cv2.waitKey(4000)
+cv2.waitKey(3000)
 cv2.destroyAllWindows()
 # https://answers.opencv.org/question/53548/gap-filling-contours-lines/
 # https://stackoverflow.com/questions/26586123/filling-gaps-in-shape-edges
